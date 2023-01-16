9bda5366e43058327b0a551c7ede12cf7a7aa437
Joydip <joydip.mondal@gmail.com>
2023-01-16 14:07:42 +0530

    app loaded

--- codes/day-1/first-app/source/index.js
+++ codes/day-1/first-app/source/index.js
@@ -1,14 +1,14 @@
-var fs = require('fs')
+//var fs = require('fs')
 function add(a, b) {
     return a + b
 }
 
 var result = add(10, 20)
-//console.log(result)
-function callback() {
-    console.log('done')
-}
-fs.writeFile('./data.txt', result.toString(), callback)
+console.log(result)
+// function callback() {
+//     console.log('done')
+// }
+// fs.writeFile('./data.txt', result.toString(), callback)
 console.log('program over')
 //window.alert(result.toString())
 // document.write(result.toString())
