dd2821e04539b01b5bbab7ac6e1575fcb61995bd
Joydip <joydip.mondal@gmail.com>
2023-01-17 15:45:14 +0530

    HTML and index file updated

---
+++ codes/day-2/posts-app/src/index.ts
@@ -0,0 +1,35 @@
+import { PostManager } from "./manager/postmanager"
+import { Post } from "./models/post"
+
+async function loadData() {
+    try {
+        const manager = new PostManager()
+        const posts = await manager.fetchPosts()
+
+        posts.forEach(
+            (p: Post) => {
+                const tdUserId = document.createElement('td')
+                tdUserId.innerText = p.userId.toString()
+
+                const tdTitle = document.createElement('td')
+                tdTitle.innerText = p.title
+
+                const tdBody = document.createElement('td')
+                tdBody.innerText = p.body
+
+                const row = document.createElement('tr')
+                row.append(tdUserId, tdTitle, tdBody)
+
+                document.getElementById('tbody')
+                    ?.appendChild(row)
+            }
+        )
+    } catch (error) {
+        const errorSpan = <HTMLSpanElement>document.getElementById('errorSpan')
+        if (errorSpan.hidden === true) {
+            errorSpan.hidden = false
+            errorSpan.innerHTML = `<b>${error.message}</b>`
+        }
+    }
+}
+loadData()
