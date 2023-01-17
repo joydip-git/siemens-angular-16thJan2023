f7aea60252dba15cf42818b0fa864e28bbdc1982
Joydip <joydip.mondal@gmail.com>
2023-01-17 14:53:57 +0530

    modular app

---
+++ codes/day-2/modular-js/source/main.ts
@@ -0,0 +1,24 @@
+//const exportableMembers = require('./utility')
+//console.log(exportableMembers.add(12, 2))
+//console.log(exportableMembers.subtract(10, 2))
+
+// const { add, subtract } = require('./utility')
+// console.log(add(12, 2))
+// console.log(subtract(12, 2))
+
+import { add, subtract } from "./utility";
+console.log(add(12, 2))
+console.log(subtract(12, 2))
+
+//object destructruing
+// const obj = {
+//     name: 'anil',
+//     id: 1,
+//     salary: 1000
+// }
+
+//const { name: name } = obj
+// const nameVal = obj.name
+// const salVal = obj.salary
+
+//console.log(name)
---
+++ codes/day-2/modular-js/source/utility.ts
@@ -0,0 +1,12 @@
+export const add = (a: number, b: number): number => a + b
+export const subtract = (a: number, b: number) => a - b
+
+// module.exports = {
+//     add: add,
+//     subtract: subtract
+// }
+// module.exports = {
+//     add,
+//     subtract
+// }
+//console.log(module)
