dc968cb8475e6578f2fc6ca12c1e243f258f0846
Joydip <joydip.mondal@gmail.com>
2023-01-18 12:11:26 +0530

    calculator update

--- codes/day-3/calculator-app/src/app/app.component.ts
+++ codes/day-3/calculator-app/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator-app';
+  first = 0
+  second = 0
+  result = 0
+  updateFirst(newFirst: number) {
+    this.first = newFirst
+  }
+  updateSecond(newSecond: number) {
+    this.second = newSecond
+  }
+  add() {
+    this.result = this.first + this.second
+  }
 }
